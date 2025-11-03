666a5a9cb7695236e2fc3f868c323a696fe157e8
Sarvesh-shrimal <sarveshshrimal2409@gmail.com>
2025-11-03 14:27:26 +0530

    added code

--- src/components/layout/SubHeader.tsx
+++ src/components/layout/SubHeader.tsx
@@ -1,14 +1,13 @@
-import type { ReactNode } from "react";
+import type { ReactNode } from 'react';
 
-interface SubHeaderProps {
-    title: ReactNode;
-}
+export default function SubHeader({ title }: { title: ReactNode }) {
 
-export default function SubHeader({ title }: SubHeaderProps) {
     return (
         <div className="bg-[#e5e5e5] w-full px-4 py-3 flex justify-between items-center shadow-sm relative z-30 h-15">
-            <div className="font-semibold flex gap-4">
-                <div className="text-xl">{title}</div>
+            <div className=" font-semibold flex gap-4">
+                <div className="text-xl">
+                    {title}
+                </div>
             </div>
         </div>
     );
