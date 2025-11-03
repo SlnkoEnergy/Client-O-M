e1347d1e71e9831ecd6cbf72d1f10e90d59f2d2a
Sarvesh-shrimal <sarveshshrimal2409@gmail.com>
2025-11-03 14:15:50 +0530

    added code

--- src/components/layout/SubHeader.tsx
+++ src/components/layout/SubHeader.tsx
@@ -1,19 +1,14 @@
-import { ChevronLeft } from "lucide-react";
+import type { ReactNode } from "react";
 
-export default function SubHeader({ title, isBackEnabled }) {
+interface SubHeaderProps {
+    title: ReactNode;
+}
 
+export default function SubHeader({ title }: SubHeaderProps) {
     return (
         <div className="bg-[#e5e5e5] w-full px-4 py-3 flex justify-between items-center shadow-sm relative z-30 h-15">
-            <div className=" font-semibold flex gap-4">
-                {isBackEnabled ? (
-                    <div>
-                        <ChevronLeft className="h-7 w-7" />
-                    </div>
-                ) : (null)}
-
-                <div className="text-xl">
-                    {title}
-                </div>
+            <div className="font-semibold flex gap-4">
+                <div className="text-xl">{title}</div>
             </div>
         </div>
     );
